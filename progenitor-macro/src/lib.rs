7934be90b9738e4ea98159d80077739312d9ced0
Adam Leventhal <ahl@oxide.computer>
2021-10-29 07:16:39 -0700

    a variety of improvements to support omicron clients (#12)
    - add the start of a client support crate
    - add support for pre/post request hooks with consumer-specific data
    - suggest type names for parameter and response types in case those
      types are unnamed
    - handle more reference types by resolving them properly
    - improve optional parameter generation

--- progenitor-macro/src/lib.rs
+++ progenitor-macro/src/lib.rs
@@ -5,7 +5,11 @@ use std::path::Path;
 use openapiv3::OpenAPI;
 use proc_macro::TokenStream;
 use progenitor_impl::Generator;
-use syn::LitStr;
+use quote::ToTokens;
+use syn::{
+    parse::{Parse, ParseStream},
+    ExprClosure, LitStr, Token,
+};
 
 #[proc_macro]
 pub fn generate_api(item: TokenStream) -> TokenStream {
@@ -15,34 +19,100 @@ pub fn generate_api(item: TokenStream) -> TokenStream {
     }
 }
 
+struct Settings {
+    file: LitStr,
+    inner: Option<proc_macro2::TokenStream>,
+    pre: Option<proc_macro2::TokenStream>,
+    post: Option<proc_macro2::TokenStream>,
+}
+
+impl Parse for Settings {
+    fn parse(input: ParseStream) -> Result<Self, syn::Error> {
+        let file = input.parse::<LitStr>()?;
+        let inner = parse_inner(input)?;
+        let pre = parse_hook(input)?;
+        let post = parse_hook(input)?;
+
+        // Optional trailing comma.
+        if input.peek(Token!(,)) {
+            let _ = input.parse::<Token!(,)>();
+        }
+
+        Ok(Settings {
+            file,
+            inner,
+            pre,
+            post,
+        })
+    }
+}
+
+fn parse_inner(
+    input: ParseStream,
+) -> Result<Option<proc_macro2::TokenStream>, syn::Error> {
+    if input.is_empty() {
+        return Ok(None);
+    }
+    let _: Token!(,) = input.parse()?;
+    if input.is_empty() {
+        return Ok(None);
+    }
+    Ok(Some(input.parse::<syn::Type>()?.to_token_stream()))
+}
+
+fn parse_hook(
+    input: ParseStream,
+) -> Result<Option<proc_macro2::TokenStream>, syn::Error> {
+    if input.is_empty() {
+        return Ok(None);
+    }
+    let _: Token!(,) = input.parse()?;
+    if input.is_empty() {
+        return Ok(None);
+    }
+    if let Ok(closure) = input.parse::<ExprClosure>() {
+        Ok(Some(closure.to_token_stream()))
+    } else {
+        Ok(Some(input.parse::<syn::Path>()?.to_token_stream()))
+    }
+}
+
 fn do_generate_api(item: TokenStream) -> Result<TokenStream, syn::Error> {
-    let arg = syn::parse::<LitStr>(item)?;
+    let Settings {
+        file,
+        inner,
+        pre,
+        post,
+    } = syn::parse::<Settings>(item)?;
     let dir = std::env::var("CARGO_MANIFEST_DIR").map_or_else(
         |_| std::env::current_dir().unwrap(),
         |s| Path::new(&s).to_path_buf(),
     );
 
-    let path = dir.join(arg.value());
+    let path = dir.join(file.value());
 
     let content = std::fs::read_to_string(&path).map_err(|e| {
         syn::Error::new(
-            arg.span(),
-            format!("couldn't read file {}: {}", arg.value(), e.to_string()),
+            file.span(),
+            format!("couldn't read file {}: {}", file.value(), e.to_string()),
         )
     })?;
 
     let spec = serde_json::from_str::<OpenAPI>(&content).map_err(|e| {
         syn::Error::new(
-            arg.span(),
-            format!("failed to parse {}: {}", arg.value(), e.to_string()),
+            file.span(),
+            format!("failed to parse {}: {}", file.value(), e.to_string()),
         )
     })?;
 
     let mut builder = Generator::new();
+    inner.map(|inner_type| builder.with_inner_type(inner_type));
+    pre.map(|pre_hook| builder.with_pre_hook(pre_hook));
+    post.map(|post_hook| builder.with_post_hook(post_hook));
     let ret = builder.generate_tokens(&spec).map_err(|e| {
         syn::Error::new(
-            arg.span(),
-            format!("generation error for {}: {}", arg.value(), e.to_string()),
+            file.span(),
+            format!("generation error for {}: {}", file.value(), e.to_string()),
         )
     })?;
 
