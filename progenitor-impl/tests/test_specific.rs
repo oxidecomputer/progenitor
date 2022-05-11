356a3ab7391b13f810a2509879083dd94931231c
Adam H. Leventhal <ahl@oxide.computer>
2022-05-10 23:15:14 -0700

    testing

--- progenitor-impl/tests/test_specific.rs
+++ progenitor-impl/tests/test_specific.rs
@@ -4,7 +4,7 @@ use std::{str::from_utf8, sync::Arc};
 
 use dropshot::{
     endpoint, ApiDescription, HttpError, HttpResponseUpdatedNoContent, Path,
-    Query, RequestContext,
+    Query, RequestContext, TypedBody,
 };
 use http::Response;
 use hyper::Body;
@@ -101,6 +101,54 @@ fn test_freeform_response() {
     let output = generator.generate_text(&spec).unwrap();
     expectorate::assert_contents(
         format!("tests/output/{}.out", "test_freeform_response"),
+        &output,
+    )
+}
+
+#[derive(Deserialize, JsonSchema)]
+#[allow(dead_code)]
+struct BodyWithDefaults {
+    s: String,
+    #[serde(default)]
+    yes: bool,
+    #[serde(default = "forty_two", rename = "forty-two")]
+    forty_two: u32,
+}
+
+fn forty_two() -> u32 {
+    42
+}
+
+#[endpoint {
+    method = POST,
+    path = "/",
+}]
+async fn default_params(
+    _rqctx: Arc<RequestContext<()>>,
+    _body: TypedBody<BodyWithDefaults>,
+) -> Result<Response<Body>, HttpError> {
+    unreachable!();
+}
+
+/// Test freeform responses.
+#[test]
+fn test_default_params() {
+    let mut api = ApiDescription::new();
+    api.register(default_params).unwrap();
+
+    let mut out = Vec::new();
+
+    api.openapi("pagination-demo", "9000")
+        .write(&mut out)
+        .unwrap();
+
+    let out = from_utf8(&out).unwrap();
+    let spec = serde_json::from_str::<OpenAPI>(out).unwrap();
+
+    let mut generator = Generator::new();
+    let output = generator.generate_text(&spec).unwrap();
+    expectorate::assert_contents(
+        format!("tests/output/{}.out", "test_default_params"),
         &output,
     )
 }
