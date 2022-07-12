7134e1afdec64038e1ac26ed3dab7903b0f57be7
karencfv <karencfv@users.noreply.github.com>
2022-07-12 20:38:37 +1200

    new doc on implementation

--- example-build/src/main.rs
+++ example-build/src/main.rs
@@ -4,24 +4,6 @@
 include!(concat!(env!("OUT_DIR"), "/codegen.rs"));
 
 fn main() {
-    // Should you need to set custom headers or other reqwest::ClientBuilder
-    // methods, you can use Client::new_with_client as shown below.
-    //
-    // let mut val = reqwest::header::HeaderValue::from_static("super-secret");
-    // val.set_sensitive(true);
-    // let mut headers = reqwest::header::HeaderMap::new();
-    // headers.insert(reqwest::header::AUTHORIZATION, val);
-    // // Insert more headers if necessary
-    //
-    // let client_builder = reqwest::ClientBuilder::new()
-    //     // Set custom timeout
-    //     .connect_timeout(Duration::new(60, 0))
-    //     // Set custom headers
-    //     .default_headers(headers)
-    //     .build()
-    //     .unwrap();
-    // let client Client::new_with_client("https://foo/bar", client_builder);
-
     let client = Client::new("https://foo/bar");
     let _ = client.enrol(&types::EnrolBody {
         host: "".to_string(),
