dcee6a4eb2c1b3e0066aa7a159708bccdae6cd2c
Adam Leventhal <ahl@oxide.computer>
2022-12-07 18:46:14 -0800

    clippy nits (#269)

--- progenitor-client/src/progenitor_client.rs
+++ progenitor-client/src/progenitor_client.rs
@@ -396,7 +396,7 @@ impl<E> RequestBuilderExt<E> for RequestBuilder {
                     "application/x-www-form-urlencoded",
                 ),
             )
-            .body(serde_urlencoded::to_string(&body).map_err(|_| {
+            .body(serde_urlencoded::to_string(body).map_err(|_| {
                 Error::InvalidRequest("failed to serialize body".to_string())
             })?))
     }
