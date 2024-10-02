5cc97b57c76bdb36f8e87224ea5a27d585dd8e78
Adam H. Leventhal <ahl@oxide.computer>
2024-10-02 15:45:31 -0700

    new test

---
+++ progenitor-impl/tests/output/src/cli_gen_cli.rs
@@ -0,0 +1,112 @@
+use crate::cli_gen_builder::*;
+pub struct Cli<T: CliConfig> {
+    client: Client,
+    config: T,
+}
+
+impl<T: CliConfig> Cli<T> {
+    pub fn new(client: Client, config: T) -> Self {
+        Self { client, config }
+    }
+
+    pub fn get_command(cmd: CliCommand) -> clap::Command {
+        match cmd {
+            CliCommand::Uno => Self::cli_uno(),
+        }
+    }
+
+    pub fn cli_uno() -> clap::Command {
+        clap::Command::new("")
+            .arg(
+                clap::Arg::new("gateway")
+                    .long("gateway")
+                    .value_parser(clap::value_parser!(String))
+                    .required(false),
+            )
+            .arg(
+                clap::Arg::new("json-body")
+                    .long("json-body")
+                    .value_name("JSON-FILE")
+                    .required(true)
+                    .value_parser(clap::value_parser!(std::path::PathBuf))
+                    .help("Path to a file that contains the full json body."),
+            )
+            .arg(
+                clap::Arg::new("json-body-template")
+                    .long("json-body-template")
+                    .action(clap::ArgAction::SetTrue)
+                    .help("XXX"),
+            )
+    }
+
+    pub async fn execute(&self, cmd: CliCommand, matches: &clap::ArgMatches) -> anyhow::Result<()> {
+        match cmd {
+            CliCommand::Uno => self.execute_uno(matches).await,
+        }
+    }
+
+    pub async fn execute_uno(&self, matches: &clap::ArgMatches) -> anyhow::Result<()> {
+        let mut request = self.client.uno();
+        if let Some(value) = matches.get_one::<String>("gateway") {
+            request = request.body_map(|body| body.gateway(value.clone()))
+        }
+
+        if let Some(value) = matches.get_one::<std::path::PathBuf>("json-body") {
+            let body_txt = std::fs::read_to_string(value).unwrap();
+            let body_value = serde_json::from_str::<types::UnoBody>(&body_txt).unwrap();
+            request = request.body(body_value);
+        }
+
+        self.config.execute_uno(matches, &mut request)?;
+        let result = request.send().await;
+        match result {
+            Ok(r) => {
+                todo!()
+            }
+            Err(r) => {
+                self.config.error(&r);
+                Err(anyhow::Error::new(r))
+            }
+        }
+    }
+}
+
+pub trait CliConfig {
+    fn success_item<T>(&self, value: &ResponseValue<T>)
+    where
+        T: schemars::JsonSchema + serde::Serialize + std::fmt::Debug;
+    fn success_no_item(&self, value: &ResponseValue<()>);
+    fn error<T>(&self, value: &Error<T>)
+    where
+        T: schemars::JsonSchema + serde::Serialize + std::fmt::Debug;
+    fn list_start<T>(&self)
+    where
+        T: schemars::JsonSchema + serde::Serialize + std::fmt::Debug;
+    fn list_item<T>(&self, value: &T)
+    where
+        T: schemars::JsonSchema + serde::Serialize + std::fmt::Debug;
+    fn list_end_success<T>(&self)
+    where
+        T: schemars::JsonSchema + serde::Serialize + std::fmt::Debug;
+    fn list_end_error<T>(&self, value: &Error<T>)
+    where
+        T: schemars::JsonSchema + serde::Serialize + std::fmt::Debug;
+    fn execute_uno(
+        &self,
+        matches: &clap::ArgMatches,
+        request: &mut builder::Uno,
+    ) -> anyhow::Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub enum CliCommand {
+    Uno,
+}
+
+impl CliCommand {
+    pub fn iter() -> impl Iterator<Item = CliCommand> {
+        vec![CliCommand::Uno].into_iter()
+    }
+}
