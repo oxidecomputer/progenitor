d8e2588ed0455fd5b7024153d8a3833247573368
Adam H. Leventhal <ahl@oxide.computer>
2025-02-02 17:00:30 -0800

    General improvements to the CLI and its docs

--- cargo-progenitor/build.rs
+++
@@ -1,8 +0,0 @@
-// Copyright 2023 Oxide Computer Company
-
-fn main() {
-    let src = project_root::get_project_root().unwrap();
-    let dst = std::path::Path::new(&std::env::var("OUT_DIR").unwrap()).join("built.rs");
-    built::write_built_file_with_opts(Some(&src), &dst)
-        .expect("Failed to acquire build-time information");
-}
